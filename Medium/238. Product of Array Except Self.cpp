f757fd8c5e0d5733801b8b7640024d6b082dbc95
Lihit <wenshaoguo0611@gmail.com>
2017-12-13 12:59:23 +0800

    new codes

--- Medium/238. Product of Array Except Self.cpp
+++ Medium/238. Product of Array Except Self.cpp
@@ -1,3 +1,7 @@
+//
+// Created by wenshao on 2017/12/13.
+//
+
 /*
 Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
 
